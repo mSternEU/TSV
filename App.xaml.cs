0ac78115e8b92cb6524fd13ec7bcbe54f6a4f03d
mst <michael.sternicki@gmail.com>
2025-06-18 13:07:19 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TSV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
